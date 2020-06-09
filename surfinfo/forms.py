6392eecec77ada64375b8ee6ef1aaa459675271b
Assaf <kremerica@gmail.com>
2020-06-08 20:57:19 -0700

    Updated icon

--- surfinfo/forms.py
+++ surfinfo/forms.py
@@ -8,12 +8,12 @@ class AddSessionForm(forms.Form):
     # surf spot choice values are ':' delimited pairs of surf spot name and surfline ID
     # should be cleaned up to something more scalable
     SURFSPOT_CHOICES = (
-        ('Pleasure Point:5842041f4e65fad6a7708807', 'Pleasure Point'),
-        ('26th Ave:5842041f4e65fad6a770898a', '26th Ave'),
         ('Steamer Lane:5842041f4e65fad6a7708805', 'Steamer Lane'),
-        ('Waddell Creek:5842041f4e65fad6a7708980', 'Waddell Creek'),
-        ('Four Mile:5842041f4e65fad6a7708981', 'Four Mile'),
+        ('26th Ave:5842041f4e65fad6a770898a', '26th Ave'),
+        ('Pleasure Point:5842041f4e65fad6a7708807', 'Pleasure Point'),
         ('The Hook:584204204e65fad6a7709996', 'The Hook'),
+        ('Four Mile:5842041f4e65fad6a7708981', 'Four Mile'),
+        ('Waddell Creek:5842041f4e65fad6a7708980', 'Waddell Creek'),
         ('Ocean Beach SF:5842041f4e65fad6a77087f8', 'Ocean Beach SF'),
         ('Linda Mar, Pacifica:5842041f4e65fad6a7708976', 'Linda Mar, Pacifica'),
         ('NEW SPOT:NONE', 'REQUEST NEW SPOT'),
