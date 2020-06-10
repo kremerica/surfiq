388fc3ad5ed67a7f0b112cee9a3ed4f9654a10a5
Assaf <kremerica@gmail.com>
2020-06-09 21:21:39 -0700

    Updated surfscore options and starting value

--- surfinfo/forms.py
+++ surfinfo/forms.py
@@ -7,26 +7,39 @@ class TimeInput(forms.TimeInput):
 class AddSessionForm(forms.Form):
     # surf spot choice values are ':' delimited pairs of surf spot name and surfline ID
     # should be cleaned up to something more scalable
-    SURFSPOT_CHOICES = (
-        ('Steamer Lane:5842041f4e65fad6a7708805', 'Steamer Lane'),
-        ('Pleasure Point:5842041f4e65fad6a7708807', 'Pleasure Point'),
-        ('The Hook:584204204e65fad6a7709996', 'The Hook'),
-        ('Four Mile:5842041f4e65fad6a7708981', 'Four Mile'),
-        ('Waddell Creek:5842041f4e65fad6a7708980', 'Waddell Creek'),
-        ('Scott Creek:5842041f4e65fad6a7708982', 'Scott Creek'),
-        ('26th Ave:5842041f4e65fad6a770898a', '26th Ave'),
-        ('Manresa:5842041f4e65fad6a770898e', 'Manresa'),
-        ('Ocean Beach SF:5842041f4e65fad6a77087f8', 'Ocean Beach SF'),
-        ('Linda Mar, Pacifica:5842041f4e65fad6a7708976', 'Linda Mar, Pacifica'),
-        ('NEW SPOT:NONE', 'REQUEST NEW SPOT'),
-    )
+    SURFSPOT_CHOICES = [
+        ('Santa Cruz County', (
+                ('Steamer Lane:5842041f4e65fad6a7708805', 'Steamer Lane'),
+                ('Pleasure Point:5842041f4e65fad6a7708807', 'Pleasure Point'),
+                ('The Hook:584204204e65fad6a7709996', 'The Hook'),
+                ('Four Mile:5842041f4e65fad6a7708981', 'Four Mile'),
+                ('Waddell Creek:5842041f4e65fad6a7708980', 'Waddell Creek'),
+                ('Scott Creek:5842041f4e65fad6a7708982', 'Scott Creek'),
+                ('26th Ave:5842041f4e65fad6a770898a', '26th Ave'),
+                ('Manresa:5842041f4e65fad6a770898e', 'Manresa'),
+            ),
+        ),
+        ('San Francisco County', (
+                ('Ocean Beach SF:5842041f4e65fad6a77087f8', 'Ocean Beach SF'),
+            ),
+        ),
+        ('San Mateo County', (
+                ('Linda Mar, Pacifica:5842041f4e65fad6a7708976', 'Linda Mar, Pacifica'),
+            ),
+        ),
+        ('None of these?', (
+                ('NEW SPOT:NONE', 'REQUEST NEW SPOT'),
+            ),
+        )
+    ]
 
     SURFSCORE_CHOICES = (
         (5, 'Legendary'),
-        (4, 'Good'),
-        (3, 'It was OK'),
-        (2, 'Not good'),
+        (4, 'Really fun'),
+        (3, 'Fun, worth repeating'),
+        (2, 'Meh, it was OK'),
         (1, 'Better off not going'),
+        (0, 'Unsurfable'),
     )
 
     CROWDSCORE_CHOICES = (
@@ -43,7 +56,7 @@ class AddSessionForm(forms.Form):
 
 #    waveCount = forms.IntegerField(label="How many waves?")
 
-    surfScore = forms.ChoiceField(label="How good?", choices=SURFSCORE_CHOICES, initial=3)
+    surfScore = forms.ChoiceField(label="How fun?", choices=SURFSCORE_CHOICES, initial=2)
     crowdScore = forms.ChoiceField(label="Crowded?", choices=CROWDSCORE_CHOICES, initial=3)
 
     def clean(self):
@@ -51,7 +64,7 @@ class AddSessionForm(forms.Form):
         endTime = self.cleaned_data['endTime']
 
         if endTime < startTime:
-            self.add_error('endTime', 'No time machines bro, end time needs to be after start time')
+            self.add_error('endTime', 'End time is before start time :(')
 
 class AddSurfSpot(forms.Form):
     spotName = forms.CharField(label="Spot name")
