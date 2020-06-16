0c39a92c9b0f9db80ff628bfe8b766cbd191a033
Assaf <kremerica@gmail.com>
2020-06-15 21:02:00 -0700

    Minor cleanup

--- surfinfo/forms.py
+++ surfinfo/forms.py
@@ -1,42 +1,43 @@
 from django import forms
-from datetime import datetime, time
+
 
 class TimeInput(forms.TimeInput):
     input_type = "time"
+
 
 class AddSessionForm(forms.Form):
     # surf spot choice values are ":" delimited pairs of surf spot name and surfline ID
     # should be cleaned up to something more scalable
     SURFSPOT_CHOICES = [
         ("Santa Cruz County", (
-                ("Steamer Lane:5842041f4e65fad6a7708805", "Steamer Lane"),
-                ("Pleasure Point:5842041f4e65fad6a7708807", "Pleasure Point"),
-                ("Sewers:5842041f4e65fad6a7708807", "Sewers"),
-                ("Rockview:5842041f4e65fad6a7708807", "Rockview"),
-                ("The Hook:584204204e65fad6a7709996", "The Hook"),
-                ("Four Mile:5842041f4e65fad6a7708981", "Four Mile"),
-                ("Waddell, Middle:5842041f4e65fad6a7708980", "Waddell, Middle"),
-                ("Waddell, South Reefs:5842041f4e65fad6a7708980", "Waddell, South Reefs"),
-                ("Scott Creek:5842041f4e65fad6a7708982", "Scott Creek"),
-                ("26th Ave:5842041f4e65fad6a770898a", "26th Ave"),
-                ("Manresa:5842041f4e65fad6a770898e", "Manresa"),
-                ("Capitola:5842041f4e65fad6a7708ddf", "Capitola"),
-            ),
+            ("Steamer Lane:5842041f4e65fad6a7708805", "Steamer Lane"),
+            ("Pleasure Point:5842041f4e65fad6a7708807", "Pleasure Point"),
+            ("Sewers:5842041f4e65fad6a7708807", "Sewers"),
+            ("Rockview:5842041f4e65fad6a7708807", "Rockview"),
+            ("The Hook:584204204e65fad6a7709996", "The Hook"),
+            ("Four Mile:5842041f4e65fad6a7708981", "Four Mile"),
+            ("Waddell, Middle:5842041f4e65fad6a7708980", "Waddell, Middle"),
+            ("Waddell, South Reefs:5842041f4e65fad6a7708980", "Waddell, South Reefs"),
+            ("Scott Creek:5842041f4e65fad6a7708982", "Scott Creek"),
+            ("26th Ave:5842041f4e65fad6a770898a", "26th Ave"),
+            ("Manresa:5842041f4e65fad6a770898e", "Manresa"),
+            ("Capitola:5842041f4e65fad6a7708ddf", "Capitola"),
         ),
+         ),
         ("San Francisco County", (
-                ("Ocean Beach, Sloat:5842041f4e65fad6a77087f8", "Ocean Beach, Sloat"),
-                ("Ocean Beach, Balboa:5842041f4e65fad6a77087f8", "Ocean Beach, Balboa"),
-                ("Ocean Beach, Noriega:5842041f4e65fad6a77087f8", "Ocean Beach, Noriega"),
+            ("Ocean Beach, Sloat:5842041f4e65fad6a77087f8", "Ocean Beach, Sloat"),
+            ("Ocean Beach, Balboa:5842041f4e65fad6a77087f8", "Ocean Beach, Balboa"),
+            ("Ocean Beach, Noriega:5842041f4e65fad6a77087f8", "Ocean Beach, Noriega"),
         ),
-        ),
+         ),
         ("San Mateo County", (
-                ("Linda Mar, Pacifica:5842041f4e65fad6a7708976", "Linda Mar, Pacifica"),
-            ),
+            ("Linda Mar, Pacifica:5842041f4e65fad6a7708976", "Linda Mar, Pacifica"),
         ),
+         ),
         ("None of these?", (
-                ("NEW SPOT:NONE", "REQUEST NEW SPOT"),
-            ),
-        )
+            ("NEW SPOT:NONE", "REQUEST NEW SPOT"),
+        ),
+         )
     ]
 
     SURFSCORE_CHOICES = (
@@ -60,7 +61,7 @@ class AddSessionForm(forms.Form):
     startTime = forms.TimeField(label="Time in", widget=TimeInput)
     endTime = forms.TimeField(label="Time out", widget=TimeInput)
 
-#    waveCount = forms.IntegerField(label="How many waves?")
+    #    waveCount = forms.IntegerField(label="How many waves?")
 
     surfScore = forms.ChoiceField(label="How fun?", choices=SURFSCORE_CHOICES, initial=2)
     crowdScore = forms.ChoiceField(label="Crowded?", choices=CROWDSCORE_CHOICES, initial=3)
@@ -78,6 +79,17 @@ class AddSurfSpot(forms.Form):
 
 
 class GetMatchingSessions(forms.Form):
+    #    SURFREGION_CHOICES = (
+    #        ("Santa Cruz County", "Santa Cruz County"),
+    #        ("San Mateo County", "San Mateo County"),
+    #        ("San Francisco County", "San Francisco County"),
+    #    )
+
+    # time + location based match
+    #    surfDatetime = forms.DateTimeField(label="When?", required=False)
+    #    surfRegion = forms.ChoiceField(label="Where?", choices=SURFREGION_CHOICES, initial=1)
+
+    # conditions based match
     swellHeight = forms.DecimalField(label="Swell height")
     swellPeriod = forms.IntegerField(label="Swell period")
     swellDirection = forms.IntegerField(label="Swell direction")
